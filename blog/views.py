f16f243f86d3562c07b634e33408b09abc286eed
AndroidHell <ralkkai1337@gmail.com>
2016-09-20 01:33:27 +0000

    fixed broken comment moeration stuff

--- blog/views.py
+++ blog/views.py
@@ -74,7 +74,7 @@ def add_comment_to_post(request, pk):
     
 @login_required
 def comment_approve(request, pk):
-    comment = get_obeject_or_404(Comment, pk=pk)
+    comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('blog.views.post_detail', pk=comment.post.pk)
     
@@ -83,5 +83,5 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
     comment.delete()
-    return redirect('blog.views.post_detail', pk=post.pk)
+    return redirect('blog.views.post_detail', pk=post_pk)
     
